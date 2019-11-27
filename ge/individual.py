9f421935eb3d98e70a03dcf5c39e5ae54f3eeb5b
revuel <revuel22@hotmail.com>
2019-11-27 22:50:51 +0100

    Adds engine module

--- ge/individual.py
+++ ge/individual.py
@@ -1,16 +1,18 @@
 """ Individual class """
 import re
 import json
+from random import randint
 from itertools import cycle
 
 
 class Individual(object):
     """ TODO: Docstring """
 
-    def __init__(self):
+    def __init__(self, grammar: dict):
+        self._grammar = grammar
         self._bin_genotype = None
-        self._int_genotype = None
-        self._fenotype = None
+        self._int_genotype = [randint(0, 255) for i in range(1,10)]
+        self._fenotype = self.decode()
         self._fitness_value = None
 
     ''' Decoding methods '''
@@ -20,7 +22,7 @@ class Individual(object):
     def translation(self):
         pass
 
-    def decode(self, grammar: dict):
+    def decode(self):
         """
             Decodes a given genotype to produce its related fenotype
             :param grammar: Grammar dict
@@ -29,26 +31,26 @@ class Individual(object):
             """
 
         done = False
-        symbolic_string = grammar["<S>"]
+        symbolic_string = self._grammar["<S>"]
         circular = cycle(self._int_genotype)
 
         while done is not True:
             old_symbolic_string = symbolic_string  # Check if anything changed from last iteration
 
-            for key in grammar.keys():
-                if type(grammar[key]) is list:
+            for key in self._grammar.keys():
+                if type(self._grammar[key]) is list:
                     ci = next(circular)
-                    fire = divmod(ci, len(grammar[key]))[1]
+                    fire = divmod(ci, len(self._grammar[key]))[1]
                     if key == '<T>':
-                        symbolic_string = re.sub(key, "{" + str(grammar[key][fire]) + "}", symbolic_string, 1)
+                        symbolic_string = re.sub(key, "{" + str(self._grammar[key][fire]) + "}", symbolic_string, 1)
                     elif key not in ['S', '<P>', '<T>', '<F>']:
                         dkey = key.replace('<', '').replace('>', '')
-                        feature = "\"" + dkey + "\"" + ":" + "\"" + str(grammar[key][fire]) + "\""
+                        feature = "\"" + dkey + "\"" + ":" + "\"" + str(self._grammar[key][fire]) + "\""
                         symbolic_string = re.sub(key, feature, symbolic_string, 1)
                     else:
-                        symbolic_string = re.sub(key, str(grammar[key][fire]), symbolic_string, 1)
+                        symbolic_string = re.sub(key, str(self._grammar[key][fire]), symbolic_string, 1)
                 else:
-                    symbolic_string = re.sub(key, str(grammar[key]), symbolic_string, 1)
+                    symbolic_string = re.sub(key, str(self._grammar[key]), symbolic_string, 1)
 
             if old_symbolic_string == symbolic_string:
                 done = True
