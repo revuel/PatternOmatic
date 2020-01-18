4ab681338bf4b9add68fb391ac749f09287725ff
revuel <revuel22@hotmail.com>
2020-01-19 00:33:35 +0100

    Adds registration of spacy token features not supported by the spacy matcher as extensions

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ from ge.population import Population
 
 ''' Engine '''
 nlp = spacy.load("en_core_web_sm")
-nlp.add_pipe(nlp.create_pipe("sentencizer"), first=True)
+nlp.add_pipe(nlp.create_pipe("sentencizer"), before="parser")
+
+print(str(nlp.pipe_names))
+
+# mydoc = nlp(u'Fuck it!')[0]
+
+# engine._set_token_extension_attributes(mydoc)
+
+a = 1
+
 
 # Receive samples
 sample_list = [nlp(u'Fuck it!'),
@@ -14,6 +23,8 @@ sample_list = [nlp(u'Fuck it!'),
                nlp(u'Fuck you!'),
                nlp(u'Fuck you all!'),
                nlp(u'Fuck!')]
+
+
 
 grammar = engine.dynagg(sample_list)
 
