928d842aa8823fdd373399709f130477498952f7
revuel <revuel22@hotmail.com>
2020-01-29 23:11:41 +0100

    Adds some tests for dgg, adds some setters for config, updates requirements and makefile to add coverage

--- settings/config.py
+++ settings/config.py
@@ -108,17 +108,33 @@ class Config(metaclass=SingletonMetaNaive):
     def population_size(self) -> int:
         return self._population_size
 
+    @population_size.setter
+    def population_size(self, value: int) -> None:
+        self.population_size = value
+
     @property
     def max_generations(self) -> int:
         return self._max_generations
+
+    @max_generations.setter
+    def max_generations(self, value: int):
+        self.max_generations = value
 
     @property
     def codon_length(self) -> int:
         return self._codon_length
 
+    @codon_length.setter
+    def codon_length(self, value) -> None:
+        self.codon_length = value
+
     @property
     def num_codons_per_individual(self) -> int:
         return self._num_codons_per_individual
+
+    @num_codons_per_individual.setter
+    def num_codons_per_individual(self, value: int) -> None:
+        self.num_codons_per_individual = value
 
     @property
     def dna_length(self) -> int:
@@ -156,33 +172,65 @@ class Config(metaclass=SingletonMetaNaive):
     def features_per_token(self) -> int:
         return self._features_per_token
 
+    @features_per_token.setter
+    def features_per_token(self, value: int) -> None:
+        self._features_per_token = value
+
     @property
     def use_boolean_features(self) -> bool:
         return self._use_boolean_features
+
+    @use_boolean_features.setter
+    def use_boolean_features(self, value: int) -> None:
+        self._use_boolean_features = value
 
     @property
     def use_custom_attributes(self) -> bool:
         return self._use_custom_attributes
 
+    @use_custom_attributes.setter
+    def use_custom_attributes(self, value: int) -> None:
+        self._use_custom_attributes = value
+
     @property
     def use_uniques(self) -> bool:
         return self._use_uniques
+
+    @use_uniques.setter
+    def use_uniques(self, value: int) -> None:
+        self._use_uniques = value
 
     @property
     def use_grammar_operators(self) -> bool:
         return self._use_grammar_operators
 
+    @use_grammar_operators.setter
+    def use_grammar_operators(self, value: int) -> None:
+        self._use_grammar_operators = value
+
     @property
     def use_token_wildcard(self) -> bool:
         return self._use_token_wildcard
+
+    @use_token_wildcard.setter
+    def use_token_wildcard(self, value) -> None:
+        self._use_token_wildcard = value
 
     @property
     def use_extended_pattern_syntax(self) -> bool:
         return self._use_extended_pattern_syntax
 
+    @use_extended_pattern_syntax.setter
+    def use_extended_pattern_syntax(self, value) -> None:
+        self._use_extended_pattern_syntax = value
+
     @property
     def fitness_function_type(self):
         return self._fitness_function_type
+
+    @fitness_function_type.setter
+    def fitness_function_type(self, value: int) -> None:
+        self._fitness_function_type = value
 
     def _check_xps_op_restriction(self):
         if self._use_extended_pattern_syntax == self._use_grammar_operators is True:
---
+++ tests/test_ge.py
@@ -0,0 +1,9 @@
+""" Unit testing file for GE module """
+import unittest
+
+
+class TestGE(unittest.TestCase):
+    """ Test class for GE """
+
+    def test(self):
+        pass
