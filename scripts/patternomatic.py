8286442096d57f16157ddfeb6244880e9b572d8c
revuel <revuel22@hotmail.com>
2020-03-01 20:52:27 +0100

    WIP adding Stats

--- scripts/patternomatic.py
+++ scripts/patternomatic.py
@@ -2,11 +2,16 @@
 """ PatternOmatic command line """
 import sys
 import argparse
-import logging
 import spacy
 from spacy.tokens.doc import Doc
+from logging import Logger
+
+from PatternOmatic.ge.stats import Stats
 from PatternOmatic.nlp.engine import dynagg as dgg
 from PatternOmatic.ge.population import Population
+
+log = Logger(__name__)
+log.setLevel(0)
 
 
 def find_pattern(text_samples: [Doc], language_model_path: str = None, configuration_path: str = None):
@@ -20,16 +25,17 @@ def find_pattern(text_samples: [Doc], language_model_path: str = None, configura
     Returns: None
 
     """
+    stats = Stats()
     bnf_g = dgg(text_samples)
-    p = Population(text_samples, bnf_g)
+    p = Population(text_samples, bnf_g, stats)
     p.evolve()
-    logging.info('Best pattern found: ', str(p.best_individual.fenotype))
-    logging.info('Score over sample: ', str(p.best_individual.fitness_value))
+    log.info('Best pattern found: ', str(p.best_individual.fenotype))
+    log.info('Score over sample: ', str(p.best_individual.fitness_value))
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    logging.info("Parsing command line arguments...")
+    log.info("Parsing command line arguments...")
     try:
         cli = argparse.ArgumentParser(description='Finds the Spacy\'s Matcher pattern for the given samples')
 
@@ -71,8 +77,8 @@ if __name__ == "__main__":
         try:
             nlp = spacy.load(parsed_args.language)
         except Exception:
-            logging.warning('Model {} not found, falling back to patternOmatic\'s default langugage model: '
-                            'en_core_web_sm'.format(parsed_args.language))
+            log.warning('Model {} not found, falling back to patternOmatic\'s default langugage model: '
+                        'en_core_web_sm'.format(parsed_args.language))
 
             nlp = spacy.load('en_core_web_sm')
 
@@ -84,5 +90,5 @@ if __name__ == "__main__":
         find_pattern(parsed_args.sample)
 
     except Exception as ex:
-        logging.critical(str(ex))
+        log.critical(str(ex))
         raise Exception(ex)
