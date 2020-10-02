2649006e9b3d76c2424a2de78beac8c839912472
revuel <revuel22@hotmail.com>
2020-10-02 18:44:34 +0200

    Updates population tests

--- tests/test_population.py
+++ tests/test_population.py
@@ -4,14 +4,17 @@ import spacy
 
 from PatternOmatic.ge.stats import Stats
 from PatternOmatic.nlp.bnf import dynamic_generator as dgg
-from PatternOmatic.ge.population import Population
+from PatternOmatic.ge.population import Population, Selection, Recombination, Replacement
 from PatternOmatic.ge.individual import Individual
 from PatternOmatic.settings.config import Config
 from PatternOmatic.settings.literals import FitnessType, SelectionType, RecombinationType, ReplacementType
 
 
-class TestPopulation(unittest.TestCase):
-    """ Unit Test class for GE Population object """
+class BasePopulationTest(unittest.TestCase):
+    """ Base class to supply shard attributes and helpers """
+    #
+    # Shared attributes
+    #
     config = Config()
 
     nlp = spacy.load("en_core_web_sm")
@@ -24,6 +27,21 @@ class TestPopulation(unittest.TestCase):
     grammar = dgg(samples)
 
     stats = Stats()
+
+    #
+    # Helpers
+    #
+    def setUp(self) -> None:
+        """ Fresh Config instance """
+        self.config = Config()
+
+    def tearDown(self) -> None:
+        """ Destroy Config instance """
+        Config.clear_instance()
+
+
+class TestPopulation(BasePopulationTest):
+    """ Unit Test class for GE Population object """
 
     def test_initialize(self):
         """ Tests that a population is correctly filled with Individuals """
@@ -107,10 +125,13 @@ class TestPopulation(unittest.TestCase):
         p.generation[0] = Individual(self.samples, self.grammar, self.stats, '01110101100101100110010110010101')
         self.config.mutation_probability = 0.5
         p.evolve()
-        super().assertGreaterEqual(p.generation[0].fitness_value, 0.2)
+        super().assertLessEqual(0.25, p.generation[0].fitness_value)
 
     def test_best_challenge_changes_best_individual(self):
         """ Covers best challenge cases """
+        self.config.mutation_probability = 0.0
+        self.config.fitness_function_type = FitnessType.BASIC
+
         p = Population(self.samples, self.grammar, self.stats)
         i1 = Individual(self.samples, self.grammar, self.stats, dna='00000000000000000000000000000000')
         i2 = Individual(self.samples, self.grammar, self.stats, dna='01110101100101100110010110010101')
@@ -120,14 +141,14 @@ class TestPopulation(unittest.TestCase):
         p.generation = [i2]
         p._best_challenge()
 
-        super().assertEqual(i2, p.generation[0])
+        super().assertEqual(p.best_individual, p.generation[0])
 
-        # When a better individual is the most fitted in a new generation, population's best individual is updated
+        # When a better individual is better fitted in a new generation, population's best individual is updated
         p.best_individual = i1
         p.generation = [i2]
         p._best_challenge()
 
-        super().assertEqual(i2, p.generation[0])
+        super().assertEqual(p.best_individual, p.generation[0])
 
         # When a worse individual is the most fitted in a new generation, population's best individual remains the same
         p.best_individual = i2
@@ -136,16 +157,71 @@ class TestPopulation(unittest.TestCase):
 
         super().assertEqual(i2, p.best_individual)
 
-    #
-    # Helpers
-    #
-    def setUp(self) -> None:
-        """ Fresh Config instance """
-        self.config = Config()
+    def test_sr_update(self):
+        """ Check SR is updated if a solution is found for the run """
+        stats = Stats()
 
-    def tearDown(self) -> None:
-        """ Destroy Config instance """
-        Config.clear_instance()
+        self.config.max_generations = 1
+        self.config.population_size = 3
+        self.config.fitness_function_type = FitnessType.BASIC
+        self.config.mutation_probability = 0.0
+
+        self.config.success_threshold = 0.0
+        p = Population(self.samples, self.grammar, stats)
+        p.generation[0] = Individual(self.samples, self.grammar, stats, '01110101100101100110010110010101')
+        p.evolve()
+        super().assertListEqual([True], stats.success_rate_accumulator)
+
+        self.config.success_threshold = 1.0
+        self.config.population_size = 1
+        p = Population(self.samples, self.grammar, stats)
+        p.generation[0] = Individual(self.samples, self.grammar, stats, '00000000000000000000000000000000')
+        p.evolve()
+        super().assertListEqual([True, False], stats.success_rate_accumulator)
+
+
+class TestSelection(BasePopulationTest):
+    """ Unit Test class for GE Selection object """
+
+    def test_dispatch(self):
+        """ Dispatcher method provides the proper selection method """
+        selection = Selection(SelectionType.BINARY_TOURNAMENT)
+        super().assertIs(selection._select, Selection._binary_tournament)
+
+        selection = Selection(SelectionType.K_TOURNAMENT)
+        super().assertIs(selection._select, Selection._k_tournament)
+
+        # Check unknown SelectionType
+        selection = Selection(None)
+        super().assertIs(selection._select, Selection._binary_tournament)
+
+
+class TestRecombination(BasePopulationTest):
+    """ Unit Test class for GE Recombination object """
+
+    def test_dispatch(self):
+        """ Dispatcher method provides the proper recombine method """
+        recombination = Recombination(self.grammar, self.samples, self.stats)
+        super().assertEqual(recombination._recombine, recombination._random_one_point_crossover)
+
+
+class TestReplacement(BasePopulationTest):
+    """ Unit Test class for GE Replacement object """
+
+    def test_dispatch(self):
+        """ Dispatcher method provides the proper replacement method """
+        replacement = Replacement(ReplacementType.MU_PLUS_LAMBDA)
+        super().assertIs(replacement._replace, Replacement._mu_plus_lambda)
+
+        replacement = Replacement(ReplacementType.MU_LAMBDA_WITH_ELITISM)
+        super().assertIs(replacement._replace, Replacement._mu_lambda_elite)
+
+        replacement = Replacement(ReplacementType.MU_LAMBDA_WITHOUT_ELITISM)
+        super().assertIs(replacement._replace, Replacement._mu_lambda_no_elite)
+
+        # Check unknown ReplacementType
+        replacement = Replacement(None)
+        super().assertIs(replacement._replace, Replacement._mu_plus_lambda)
 
 
 if __name__ == "__main__":
